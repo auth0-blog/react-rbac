f160b0f73ac27bff7ff86cbc54e6d6597c3f3a37
Bruno Krebs <bruno.krebs@auth0.com>
2018-09-17 18:34:25 -0300

    ### Implement Can component

---
+++ src/components/Can.js
@@ -0,0 +1,41 @@
+import rules from "../rbac-rules";
+
+const check = (rules, role, action, data) => {
+  const permissions = rules[role];
+  if (!permissions) {
+    // role is not present in the rules
+    return false;
+  }
+
+  const staticPermissions = permissions.static;
+
+  if (staticPermissions && staticPermissions.includes(action)) {
+    // static rule not provided for action
+    return true;
+  }
+
+  const dynamicPermissions = permissions.dynamic;
+
+  if (dynamicPermissions) {
+    const permissionCondition = dynamicPermissions[action];
+    if (!permissionCondition) {
+      // dynamic rule not provided for action
+      return false;
+    }
+
+    return permissionCondition(data);
+  }
+  return false;
+};
+
+const Can = props =>
+  check(rules, props.role, props.perform, props.data)
+    ? props.yes()
+    : props.no();
+
+Can.defaultProps = {
+  yes: () => null,
+  no: () => null
+};
+
+export default Can;
